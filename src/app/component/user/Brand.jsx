9d2293f804592634fcaac12df263d78559296ce2
haimomo24 <135099315+haimomo24@users.noreply.github.com>
2024-11-15 17:05:19 +0700

    update

--- src/app/component/user/Brand.jsx
+++ src/app/component/user/Brand.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Image from 'next/image'  // Import Image từ next/image
 
 const Brand = () => {
     const logos = [  
@@ -8,20 +9,22 @@ const Brand = () => {
         { src: 'https://www.tncstore.vn/media/brand/11.png', alt: 'Acer' },  
         { src: 'https://www.tncstore.vn/media/brand/78.jpg', alt: 'Sony' },  
         { src: 'https://www.tncstore.vn/media/brand/28.png', alt: 'LG' },  
-        
-      ]; 
-  return (
-    <div className="flex justify-center mt-[30px] mb-[30px] items-center space-x-6 p-4 bg-white">  
-    {logos.map((logo, index) => (  
-      <img  
-        key={index}  
-        src={logo.src}  
-        alt={logo.alt}  
-        className="h-12" // Adjust height as necessary  
-      />  
-    ))}  
-  </div>  
-  )
+    ]; 
+
+    return (
+        <div className="flex justify-center mt-[30px] mb-[30px] items-center space-x-6 p-4 bg-white">  
+            {logos.map((logo, index) => (  
+                <Image  
+                    key={index}  
+                    src={logo.src}  
+                    alt={logo.alt}  
+                    width={80}  // Đặt chiều rộng cho ảnh
+                    height={40} // Đặt chiều cao cho ảnh
+                    className="object-contain" // Đảm bảo ảnh không bị vỡ hoặc méo
+                />  
+            ))}  
+        </div>  
+    )
 }
 
 export default Brand
--- src/app/component/user/common/Header.jsx
+++ src/app/component/user/common/Header.jsx
@@ -1,22 +1,28 @@
 import React from 'react'
+import Image from 'next/image'  // Import Image từ next/image
 
 const Header = () => {
   return (
     <div className="flex items-center justify-between p-4 bg-white">  
-    <div className="flex items-center space-x-6">  
-        <img src="https://www.asus.com/media/Odin/images/header/ROG_hover.svg" alt="Logo" className="h-8 w-auto" />  
+      <div className="flex items-center space-x-6">  
+        <Image 
+          src="https://www.asus.com/media/Odin/images/header/ROG_hover.svg" 
+          alt="Logo" 
+          width={32}  // Thiết lập chiều rộng (px)
+          height={32} // Thiết lập chiều cao (px)
+          className="w-auto h-8" // Bạn vẫn có thể sử dụng class Tailwind cho các kiểu dáng khác
+        />  
         <div className="flex space-x-6 text-gray-600">  
             <span>ProArt</span>  
             <span>ASUS IoT</span>  
         </div>  
-    </div>  
-    <div className="flex space-x-6 text-gray-600">  
+      </div>  
+      <div className="flex space-x-6 text-gray-600">  
         <span>Sự kiện</span>  
         <span>Gaming</span>  
         <span>Doanh Nghiệp</span>  
+      </div>  
     </div>  
-</div>  
-
   )
 }
 
